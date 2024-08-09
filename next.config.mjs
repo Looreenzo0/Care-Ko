ee7ce498996057eea45f0140a1114a700c58b5d3
John Laurence Resando <83331866+Looreenzo0@users.noreply.github.com>
2024-08-09 12:05:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.mjs
+import pkg from "next";
+
+const { nextConfig } = pkg;
+
+const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: ["example.com"],
+  },
+  webpack(config, options) {
+    // Customize the webpack configuration here
+    return config;
+  },
+  // Add other configurations here
 };
 
-// module.exports = nextConfig;
+export default config;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreBuildErrors: true,
+//   },
+// };
